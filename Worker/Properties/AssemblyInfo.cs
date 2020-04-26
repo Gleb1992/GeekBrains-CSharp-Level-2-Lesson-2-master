7698f595221e95a0b07afb16b7f8c10c98891940
Gleb Gribkov <ggv@begarat.com>
2020-04-26 11:04:02 +0300

    Добавьте файлы проекта.

---
+++ Worker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Worker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KrotySOFT")]
+[assembly: AssemblyProduct("Worker")]
+[assembly: AssemblyCopyright("Copyright © KrotySOFT 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("11c47dc3-84b2-4ad5-8169-84ad49a7516e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Worker/Salary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Worker
+{
+    abstract class Salary
+    {
+        protected string fio;
+        protected double salary; //хранится зарплата за месяц или за час
+
+        public Salary(string fio, double salary)
+        {
+            this.fio = fio;
+            this.salary = salary;
+        }
+
+        abstract public double CalcSalary();
+
+        override public string ToString()
+        {
+
+            return this.fio + "  зп = " + this.CalcSalary();
+        } 
+    }
+}
